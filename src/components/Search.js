f231f6b0a426cf76289a10f1a3e7125b8a6beea5
Emre <emrem5979@gmail.com>
2020-06-13 17:56:21 +0300

    added most expensive cars and most cheap cars options

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
-import {Button} from 'reactstrap';
+import {Button, Container, Row} from 'reactstrap';
+import ShowCars from './ShowCars';
+import axios from 'axios';
 
 class Search extends Component {
-    //TODO search options need to add
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            cars:[]
+        }
+    }
+    onClickExpensive = e =>{
+        axios.get('http://localhost:3000/cars/sortByMostExpensive')
+        .then((res) => {
+            this.setState({
+                cars: res.data
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+        console.log('most expensive cars is fetched ! !')
+    }
+
+    onClickCheap = e =>{
+        axios.get('http://localhost:3000/cars/sortByMostCheap')
+        .then((res) => {
+            this.setState({
+                cars: res.data
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+        console.log('most cheap cars is fetched ! !')
+
+    }
+
     render() {
+        const {cars} = this.state;
         return (
-            <div>
-                <Button className="m-3" color="primary">Most Expensive</Button>
-                <Button className="m-3" color="primary">Most Cheap</Button>
-            </div>
+            <Container>
+               <Button onClick={this.onClickExpensive} className="m-3" color="primary">Most Expensive</Button>
+                <Button onClick={this.onClickCheap} className="m-3" color="primary">Most Cheap</Button>
+                    <ShowCars cars={cars} />
+            </Container>
         )
     }
 }
